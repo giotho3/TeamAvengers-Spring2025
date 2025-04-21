45c86d31296422162a11b3892fc1167c2fb9eebb
giotho3 <giothomas02@gmail.com>
2025-04-20 21:44:33 -0400

    fight and grimoire commands added to main logic

--- src/View/HelpMenu.java
+++ src/View/HelpMenu.java
@@ -14,12 +14,13 @@ public class HelpMenu {
     }
 
     private void initializeHelp() {
-        helpSections.put("navigation", "Use commands: North, East, South, West, Enter, Exit, Look Around.");
+        helpSections.put("navigation", "Use commands: N(orth), S(outh), E(ast), W(est), and Look Around to move around the map");
         helpSections.put("puzzle", "Interact with puzzles using the 'interact' command. Request hints with 'hint'.");
         helpSections.put("combat", "Use 'fight' to engage, 'attack' to deal damage, and 'flee' to escape battles.");
-        helpSections.put("items", "Equip weapons, armor, potions via inventory management. Use 'items' command.");
+        helpSections.put("items", "Equip weapons, armor, potions and spells via inventory management. Use '<wear, drink, conjure, equip> + <item name>' command.");
         helpSections.put("story", "Your journey unfolds as you explore and survive against challenges.");
         helpSections.put("save", "Manually save progress in designated rooms. Use 'save' command when prompted.");
+        helpSections.put("exit", "Exits the help menu");
     }
 
     public void displayHelp(String topic) {
